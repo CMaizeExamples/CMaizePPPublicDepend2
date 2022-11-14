07e4afba85c1b18fabdf3f6171d4a79b09f79404
zachcran <zachcran@gmail.com>
2022-11-14 14:45:35 -0600

    Fix naming issue

--- src/cmaize_public_depend_2/cmaize_public_depend_2.cpp
+++ src/cmaize_public_depend_2/cmaize_public_depend_2.cpp
@@ -1,5 +1,5 @@
 #include "cmaize_public_depend_2/cmaize_public_depend_2.hpp"
 
-int call_cmake_public_depend_2() {
-    return call_cmake_public_depend() + 2;
+int call_cmaize_public_depend_2() {
+    return call_cmaize_public_depend() + 2;
 }
