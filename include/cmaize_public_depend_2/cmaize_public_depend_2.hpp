6205755d2eed19e8fe26342bc905c216e7bb40f5
zachcran <zachcran@gmail.com>
2022-11-14 14:43:16 -0600

    Add include and src directories with library code

---
+++ include/cmaize_public_depend_2/cmaize_public_depend_2.hpp
@@ -0,0 +1,6 @@
+#ifndef __CMAIZE_PUBLIC_DEPEND_2
+#define __CMAIZE_PUBLIC_DEPEND_2
+
+#include <cmaize_public_depend/cmaize_public_depend.hpp>
+
+#endif // __CMAIZE_PUBLIC_DEPEND_2
---
+++ src/cmaize_public_depend_2/cmaize_public_depend_2.cpp
@@ -0,0 +1,5 @@
+#include "cmaize_public_depend_2/cmaize_public_depend_2.hpp"
+
+int call_cmake_public_depend2() {
+    return call_cmake_public_depend() + 2;
+}
