460622f6cb9649602b40b55911d8c4a8c7aee243
zachcran <zachcran@gmail.com>
2023-04-09 19:11:37 -0500

    Added namespace

--- src/cmaize_public_depend_2/cmaize_public_depend_2.cpp
+++ src/cmaize_public_depend_2/cmaize_public_depend_2.cpp
@@ -1,5 +1,9 @@
 #include "cmaize_public_depend_2/cmaize_public_depend_2.hpp"
 
+namespace cmaize_public_depend_2 {
+
 int call_cmaize_public_depend_2() {
-    return call_cmaize_public_depend() + 2;
+    return cmaize_public_depend::call_cmaize_public_depend() + 2;
 }
+
+} // namespace cmaize_public_depend_2
